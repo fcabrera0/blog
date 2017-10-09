6d96f180bdf41df7af53f0b3664b668f75d195d5
Felipe Cabrera <fcabrera@mskr.io>
2017-10-09 16:45:18 -0300

    eh

--- models.rb
+++ models.rb
@@ -2,28 +2,37 @@ require 'mongoid'
 
 class User
   include Mongoid::Document
+
   field :email, type: String
   field :name, type: String
   field :role, type: Array, default: ['reader']
   field :password, type: Hash
   field :timestamp, type: DateTime, default: ->{ DateTime.now }
+
   has_many :sessions
   has_many :posts
+
+  accepts_nested_attributes_for :posts
+  accepts_nested_attributes_for :sessions
 end
 
 class Session
   include Mongoid::Document
+
   field :ip, type: String
   field :timestamp, type: DateTime, default: ->{ DateTime.now }
+
   belongs_to :user
 end
 
 class Post
   include Mongoid::Document
+
   field :title, type: String
   field :content, type: String
   field :tags, type: Array
   field :timestamp, type: DateTime, default: ->{ DateTime.now }
+
   belongs_to :user
 end
 
--- routes.rb
+++ routes.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
 require 'sinatra/cookies'
+require 'mongoid'
+require 'base64'
+require 'digest'
 require_relative 'models'
 
+Mongoid.load!('mongoid.yml', :production)
 set :port, 3000
 
 before do
@@ -15,25 +19,71 @@ end
 
 # Home
 get '/' do
-  @title = 'Home'
+  @title = 'Inicio'
   erb :home
 end
 
-# Signup/login page
-get '/account' do
-  @title = 'Account'
-  erb :account
+get '/contacto' do
+  @title = 'Contacto'
+  erb :contact
+end
+
+# Login page
+get '/login' do
+  @title = 'Login'
+  erb :login
+end
+
+# Signup
+get '/signup' do
+  @title = 'Signup'
+  erb :signup
 end
 
 get '/post/:id' do
-
+  begin
+    @post = Post.find(params[:id])
+    @title = @post.title
+  rescue Mongoid::Errors::DocumentNotFound
+    @post = nil
+    @title = 'Publicación no encontrada'
+  end
+  erb :post
 end
 
+# Signup
 post '/auth/new' do
+  [:email, :password].each do |e|
+    return {:success=>0, :code=>1}.to_json unless params.include? e
+  end
+  return {:success=>0, :code=>2}.to_json if User.where(:email=>params[:email]).exists
 
-  {:success=>1}.to_json
+  salt = SecureRandom.base64
+  value = Digest::SHA2.new(512).hexdigest(params[:password] + salt)
+
+  user = User.create(
+      email: params[:email],
+      password: { salt: salt, value: value}
+  )
+
+  {:success=>1, :id=>user.id.to_s}.to_json
 end
 
+# Login
 post '/auth/open' do
-  {:success=>1}.to_json
+  [:email, :password].each do |e|
+    return {:success=>0, :code=>1}.to_json unless params.include? e
+  end
+  return {:success=>0, :code=>2}.to_json unless User.where(:email=>params[:email]).exists
+
+  user = User.where(email: params[:email]).first
+  salt = user.password[:salt]
+  value = Digest::SHA2.new(512).hexdigest(params[:password] + salt)
+
+  return {:success=>0, :code=>3}.to_json unless value == user.password[:value]
+
+  session = user.sessions.build(ip: request.ip)
+  user.save
+
+  {:success=>1, :id=>session.id.to_s}.to_json
 end
